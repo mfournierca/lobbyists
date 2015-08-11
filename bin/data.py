0ceb79e2644a2f1a57c86646b9e2febe692dc444
Matthew Fournier <fournier.matthew@gmail.com>
2015-08-11 12:36:52 -0400

    Refactor data loading to remove duplicated code
    Issue: master

--- bin/data.py
+++ bin/data.py
@@ -58,103 +58,96 @@ def load_all(commit_interval=10000):
     load_client(session, commit_interval=commit_interval)
 
 
-def load_client(session, commit_interval=10000):
-    logbook.info("loading client data")
-    with open(join(SOURCE_DATA_ROOT, "CLIENT_NMExport.csv")) as w:
+def _load(session, csvfile, row_creator, commit_interval=10000):
+    with open(csvfile) as w:
         r = csv.reader(w)
         header = r.next()
         b = 0
         for row in r:
-            row = [unicode(i, errors="replace") for i in row[:2]]
-            session.add(db.Client(
-                client_num=row[0],
-                client_name=row[1]
-            ))
+            row = [unicode(i, errors="replace") for i in row if i is not None]
+            session.merge(row_creator(row))
             b += 1
             if b % commit_interval == 0:
                 session.commit()
                 logbook.debug("committed {0} rows".format(b))
         session.commit()
         logbook.debug("committed {0} rows".format(b))
+
+
+def load_client(session, commit_interval=10000):
+    logbook.info("loading client data")
+
+    row_creator = lambda row: db.Client(
+        client_num=row[0], client_name=row[1]
+    )
+
+    _load(
+        session,
+        join(SOURCE_DATA_ROOT, "CLIENT_NMExport.csv"),
+        row_creator,
+        commit_interval=commit_interval
+    )
 
 
 def load_communication_registrant(session, commit_interval=10000):
     logbook.info("loading communication registrant data")
-    with open(
-        join(SOURCE_DATA_ROOT, "Communication_PrimaryExport.csv")
-    ) as w:
-        r = csv.reader(w)
-        header = r.next()
-        b = 0
-        for row in r:
-            row = [i.decode("utf-8") for i in row]
-            session.add(db.CommunicationRegistrant(
-                comlog_id=row[0],
-                client_num=row[1],
-                registrant_num=row[2],
-                registrant_last_name=row[3],
-                registrant_first_name=row[4],
-                com_date=datetime.strptime(row[5], STRPTIME_FORMAT),
-                reg_type=row[6],
-                submission_date=datetime.strptime(row[7], STRPTIME_FORMAT),
-                posted_date=datetime.strptime(row[8], STRPTIME_FORMAT)
-            ))
-            b += 1
-            if b % commit_interval == 0:
-                session.commit()
-                logbook.debug("committed {0} rows".format(b))
-        session.commit()
-        logbook.debug("committed {0} rows".format(b))
+
+    row_creator = lambda row: db.CommunicationRegistrant(
+        comlog_id=row[0],
+        client_num=row[1],
+        registrant_num=row[2],
+        registrant_last_name=row[3],
+        registrant_first_name=row[4],
+        com_date=datetime.strptime(row[5], STRPTIME_FORMAT),
+        reg_type=row[6],
+        submission_date=datetime.strptime(row[7], STRPTIME_FORMAT),
+        posted_date=datetime.strptime(row[8], STRPTIME_FORMAT)
+    )
+
+    _load(
+        session,
+        join(SOURCE_DATA_ROOT, "Communication_PrimaryExport.csv"),
+        row_creator,
+        commit_interval=commit_interval
+    )
 
 
 def load_communication_dpoh(session, commit_interval=10000):
     logbook.info("loading dpoh communication data")
-    with open(
-        join(SOURCE_DATA_ROOT, "Communication_DPOHExport.csv")
-    ) as w:
-        r = csv.reader(w)
-        header = r.next()
-        b = 0
-        for row in r:
-            row = [i.decode("utf-8") for i in row]
-            session.add(db.CommunicationDPOH(
-                comlog_id=row[0],
-                dpoh_last_name=row[1],
-                dpoh_first_name=row[2],
-                dpoh_title=row[3],
-                branch_unit=row[4],
-                other_institution=row[5],
-                institution=row[6]
-            ))
-            b += 1
-            if b % commit_interval == 0:
-                session.commit()
-                logbook.debug("committed {0} rows".format(b))
-        session.commit()
-        logbook.debug("committed {0} rows".format(b))
+
+    row_creator = lambda row: db.CommunicationDPOH(
+        comlog_id=row[0],
+        dpoh_last_name=row[1],
+        dpoh_first_name=row[2],
+        dpoh_title=row[3],
+        branch_unit=row[4],
+        other_institution=row[5],
+        institution=row[6]
+    )
+
+    _load(
+        session,
+        join(SOURCE_DATA_ROOT, "Communication_DPOHExport.csv"),
+        row_creator,
+        commit_interval=commit_interval
+    )
 
 
 def load_subject_matter(session, commit_interval=10000):
     logbook.info("loading subject matter data")
-    with open(
-        join(SOURCE_DATA_ROOT, "Communication_SubjectMattersExport.csv")
-    ) as w:
-        r = csv.reader(w)
-        header = r.next()
-        b = 0
-        for row in r:
-            row = [i.decode("utf-8") for i in row]
-            session.add(db.SubjectMatter(
-                comlog_id=row[0],
-                subject_matter=row[1],
-                other_subject_matter=row[1]
-            ))
-            b += 1
-            if b % commit_interval == 0:
-                session.commit()
-                logbook.debug("committed {0} rows".format(b))
-        session.commit()
-        logbook.debug("committed {0} rows".format(b))
+
+    row_creator = lambda row: db.SubjectMatter(
+        comlog_id=row[0],
+        subject_matter=row[1],
+        other_subject_matter=row[1]
+    )
+
+    _load(
+        session,
+        join(SOURCE_DATA_ROOT, "Communication_SubjectMattersExport.csv"),
+        row_creator,
+        commit_interval=commit_interval
+    )
 
 
 if __name__ == "__main__":
