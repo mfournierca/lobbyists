070aad695b4182d07f5623e222726335c06b3645
Matthew Fournier <fournier.matthew@gmail.com>
2015-08-10 22:20:20 -0400

    add foreign keys
    Issue: master

--- src/db.py
+++ src/db.py
@@ -3,6 +3,7 @@ from src.constants import DATA_ROOT
 
 from sqlalchemy import Column, ForeignKey, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 SQLITE_DB_PATH = join(DATA_ROOT, "sqlite.db")
@@ -21,7 +22,11 @@ class SubjectMatter(Base):
 class CommunicationRegistrant(Base):
     __tablename__ = "communication_registrant"
     comlog_id = Column(Integer, nullable=False, primary_key=True)
-    client_num = Column(Integer, nullable=False)
+    client_num = Column(
+        Integer,
+        ForeignKey("client.client_num"),
+        nullable=False
+    )
     registrant_num = Column(Integer, nullable=False)
     registrant_last_name = Column(String, nullable=False)
     registrant_first_name = Column(String, nullable=False)
@@ -34,7 +39,12 @@ class CommunicationRegistrant(Base):
 class CommunicationDPOH(Base):
     __tablename__ = "communication_dpoh"
     id = Column(Integer, autoincrement=True, primary_key=True)
-    comlog_id = Column(Integer, nullable=False)
+    comlog_id = Column(
+        Integer,
+        ForeignKey("communication_registrant.comlog_id"),
+        ForeignKey("subject_matter.comlog_id"),
+        nullable=False
+    )
     dpoh_last_name = Column(String, nullable=False)
     dpoh_first_name = Column(String, nullable=False)
     dpoh_title = Column(String, nullable=False)
@@ -52,3 +62,7 @@ class Client(Base):
 
 engine = create_engine("sqlite:///{0}".format(SQLITE_DB_PATH))
 Base.metadata.create_all(engine)
+
+
+def make_session():
+    return sessionmaker(bind=engine)()
