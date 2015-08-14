48c8ab8adf3b4d26196125316b70d8e95c0cfc2d
Matthew Fournier <fournier.matthew@gmail.com>
2015-08-13 22:34:06 -0400

    limit query
    Issue: master

--- src/sql/dph_com_dates.sql
+++ src/sql/dph_com_dates.sql
@@ -2,6 +2,6 @@ SELECT
 	DISTINCT(cr.com_date)
 FROM communication_dpoh dpoh
 	INNER JOIN communication_registrant cr ON cr.comlog_id == dpoh.comlog_id
-WHERE dpoh.dpoh_last_name == "Harper"
+WHERE dpoh.dpoh_last_name == "Harper" AND dpoh.dpoh_first_name == "Stephen"
 ORDER BY cr.com_date
 LIMIT 50
