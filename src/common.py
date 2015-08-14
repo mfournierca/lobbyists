bde39ad3e9da4e0e9af6b1514d54ae0121711dad
Matthew Fournier <fournier.matthew@gmail.com>
2015-08-13 23:17:34 -0400

    remove honorifics from mp names
    Issue: master

--- src/common.py
+++ src/common.py
@@ -1,5 +1,17 @@
+REPLACE = [
+    "Right Hon.",
+    "Hon.",
+    "Right Honorable",
+    "Honorable"
+]
+
+
 def clean_name(name):
     """Standard name-cleaning routine. Should accept any number of
     space-delimited words."""
-    return name.title()
+
+    for i in REPLACE:
+        name = name.replace(i, "")
+    name = name.title()
+    return name
 
