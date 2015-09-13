a795bf8975ed736b2fdaa189f0e0998e3f742056
Matthew Fournier <fournier.matthew@gmail.com>
2015-09-13 16:23:39 -0400

    move data cleaning into src/data/
    Issue: master

--- src/data/clean.py
+++ src/data/clean.py
@@ -1,86 +1,43 @@
-import pandas
-import numpy
+import re
+
+import pandas as pd
 import Levenshtein
-from sklearn import cluster
-import Levenshtein
 
 
-def find_correct_names(names):
-    """Given a list of (lastname, firstname, count) tuples find the correct
-    spelling of each name.
+PUNCTUATION = [
+    ".",
+    ","
+]
 
-    We accomplish this in 2 steps:
+REPLACE = [
+    "the",
+    "right",
+    "rt",
+    "hon",
+    "honorable",
+    "honourable",
+    "mp",
+    "pc"
+]
 
-    - Cluster the names so that different spellings are in the same cluster
-    - Take the most frequent spelling from each cluster and consider it correct
-
-    Return a dataframe containing the original and correct name on each row.
-    """
-
-    df = pandas.DataFrame(names, columns=["lastname", "firstname", "count"])
-    df["name"] = df["lastname"] + df["firstname"]
-
-    df = _cluster_and_label(df, column="name")
-
-    # find the index of the max count within each label
-    correct = df.groupby("label")["count"].idxmax()
-    correct = pandas.DataFrame(correct)
-    correct.columns = ["maxcount_index"]
-    correct["label"] = correct.index
-
-    # add the correct name on each row
-    df = pandas.merge(df, correct, how="inner", on="label")
-    df["correct_firstname"] = df.apply(
-        lambda row: df.ix[row["maxcount_index"]]["firstname"],
-        axis=1
-    )
-    df["correct_lastname"] = df.apply(
-        lambda row: df.ix[row["maxcount_index"]]["lastname"],
-        axis=1
-    )
-
-    return df
+SUB = ["^" + r + "\s+" for r in REPLACE]
+SUB += ["\s+" + r + "\s+" for r in REPLACE]
 
 
-def _cluster_and_label(df, column="name", label="label"):
-    """Cluster a dataframe on the given column and add a "label" column"""
+def clean_name(name):
+    """Clean the provided name. Capitalize properly, remove punctuation,
+    titles and pronouns. """
 
-    # remove non-ascii characters, sklearn crashes
-    df[column] = df[column].apply(
-        lambda x: "".join([i for i in x if 0 < ord(i) < 127])
-    )
-
-    dist = _build_distance_matrix(df)
-
-    dbscan = cluster.DBSCAN(eps=3, metric="precomputed", min_samples=1)
-    labels = dbscan.fit_predict(dist)
-
-    df[label] = labels
-    return df
+    name = name.lower()
+    name = name.strip()
+    for p in PUNCTUATION:
+        name = name.replace(p, "")
+    for i in SUB:
+        name = re.sub(i, "", name)
+    name = name.title()
+    return name
 
 
-def _build_distance_matrix(
-        df,
-        col="name",
-        width=10,
-        default_dist_value=10000
-    ):
-    dist = numpy.ndarray((len(df), len(df)))
-    dist.fill(default_dist_value)
-    for i, n in enumerate(df[col]):
-        if i - width <= 0:
-            lower = 0
-        else:
-            lower = i - width
+def clean_last_and_first_name(lastname, firstname):
+    return clean_name(lastname), clean_name(firstname)
 
-        if i + width >= len(df[col]):
-            upper = len(df[col]) - 1
-        else:
-            upper = i + width
-
-        for j, m in enumerate(df[col][lower:upper + 1]):
-            pos = j + lower
-            d = Levenshtein.distance(n, m)
-            dist[i][pos] = d
-
-    return dist
