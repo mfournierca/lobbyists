569d744bc07a979a3e524fca0aa15c4bc588476a
Matthew Fournier <fournier.matthew@gmail.com>
2015-09-24 23:32:09 -0400

    bugfixes and filtering in lobbyists api
    Issue: itinerary_api

--- src/api/lobbyists.py
+++ src/api/lobbyists.py
@@ -10,19 +10,19 @@ Options:
 import logbook
 
 from docopt import docopt
-from flask import Flask, Response
+from flask import Flask, Response, request
 from flask.ext.cors import CORS
-from src import db
+from src.db import db, util
 from json import dumps
 from sqlalchemy import asc
 from collections import defaultdict
 
-from src.common import clean_name
+from src.data.clean import clean_name
 
 APP = Flask(__name__)
 CORS(APP)
 
-SESSION = db.make_sqlalchemy_session()
+SESSION = util.make_sqlalchemy_session()
 BASE_PATH = "/api/v1/{0}"
 PORT = 5001
 
@@ -32,30 +32,42 @@ def publicservant_itinerary(lastname=None, firstname=None, limit=100):
     lastname = clean_name(lastname) if lastname else ""
     firstname = clean_name(firstname) if firstname else ""
 
-    query = SESSION.query(db.DPOHCommDetailsView)
-    query = query.filter(db.DPOHCommDetailsView.dpoh_last_name == lastname)
-    query = query.filter(db.DPOHCommDetailsView.dpoh_first_name == firstname)
-    query.order_by(db.DPOHCommDetailsView.com_date)
-    query.limit(limit)
+    import pdb
+    pdb.set_trace()
 
-    data = {
-        "dpoh_last_name": lastname,
-        "dpoh_first_name": firstname,
-        "itinerary": defaultdict(list)
+    query = SESSION.query(db.DPOHCommDetailsView)
+    query = query.filter_by(dpoh_last_name=lastname)
+    query = query.filter_by(dpoh_first_name=firstname)
+    query = query.order_by(db.DPOHCommDetailsView.com_date)
+    query = query.limit(limit)
+    results = query.all()
+
+    response = {
+        "message": None,
+        "data": {
+            "dpoh_last_name": lastname,
+            "dpoh_first_name": firstname,
+            "limit": limit,
+            "count": len(results),
+            "itinerary": defaultdict(list)
+        }
     }
-    for r in query.all():
-        data["itinerary"][r.com_date_str()].append({
+    for r in results:
+        response["data"]["itinerary"][r.com_date_str()].append({
             "reg_first_name": r.registrant_first_name,
             "reg_last_name": r.registrant_last_name,
             "comlog_id": r.comlog_id,
             "subject_matter": r.subject_matter,
             "client_name": r.client_name
         })
-    return data
+    return response
 
 
 @APP.route(BASE_PATH.format("publicservant/<lastname>_<firstname>/itinerary"))
-def _publicservant_itinerary(lastname=None, firstname=None, limit=100):
+def _publicservant_itinerary(lastname=None, firstname=None):
+    limit = request.args.get("limit", 100)
+    limit = int(limit)
+
     data = publicservant_itinerary(
         lastname=lastname,
         firstname=firstname,
